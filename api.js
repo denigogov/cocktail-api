40bd244701639a7918521909e458624a4cb539ca
denigogov <122295314+denigogov@users.noreply.github.com>
2023-03-29 01:19:49 +0200

    add by season filter

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const drinks = [
   {
     name: "Margarita",
-    id: "22",
+    id: "1",
     category: "Coctail",
     alcoholic: "Alcoholic",
     image:
@@ -24,7 +24,7 @@ const drinks = [
 
   {
     name: "Mojito",
-    id: "2233",
+    id: "2",
     category: "Coctail",
     alcoholic: "Alcoholic",
     image:
@@ -40,12 +40,12 @@ const drinks = [
     ingredients4: "6 leaves of Min",
     ingredients5: "Soda Water",
     ingredients6: "Ice",
-    season: "summer",
+    season: "autumn",
   },
 
   {
     name: "Espresso martini",
-    id: "2233111",
+    id: "3",
     category: "Coctail",
     alcoholic: "Alcoholic",
     image:
@@ -61,6 +61,69 @@ const drinks = [
     ingredients4: "3 Whole Coffee Beans",
     ingredients5: "Ice",
     season: "winter",
+  },
+
+  {
+    name: "Hot Toddy",
+    id: "4",
+    category: "Coctail",
+    alcoholic: "Alcoholic",
+    image:
+      "https://cdn.vectorstock.com/i/1000x1000/02/37/wassail-hot-apple-cider-traditional-winter-vector-39380237.webp",
+    description:
+      "A hot toddy, also known as hot whiskey in Ireland, is typically a mixed drink made of liquor and water with honey, lemon, herbs and spices, and served hot. Recipes vary, and hot toddy is traditionally drunk before retiring for the night, in wet or cold weather or to relieve the symptoms of the cold and flu. In How to Drink, Victoria Moore describes the drink as 'the vitamin C for health, the honey to soothe, the alcohol to numb.' ",
+    instructions:
+      "A hot toddy is a mixture of a spirit (usually whisky), hot water, and honey (or, in some recipes, sugar). In Canada, maple syrup may be used. Additional ingredients such as cloves, a lemon slice or cinnamon (in stick or ground form) are often also added.",
+
+    ingredients1: "whiskey",
+    ingredients2: "¾ cup water",
+    ingredients3: "2 to 3 teaspoons honey, to taste",
+    ingredients4: "2 to 3 teaspoons lemon juice, to taste",
+    ingredients5: "1 lemon round",
+    ingredients6: "1 cinnamon stick",
+    season: "winter",
+  },
+
+  {
+    name: "Mulled Wine",
+    id: "5",
+    category: "Coctail",
+    alcoholic: "Alcoholic",
+    image:
+      "https://cdn.vectorstock.com/i/1000x1000/12/03/composition-on-white-with-mulled-wine-vector-12181203.webp",
+    description:
+      "Mulled wine, also known as spiced wine, is an alcoholic drink usually made with red wine, along with various mulling spices and sometimes raisins, served hot or warm. It is a traditional drink during winter, especially around Christmas. It is usually served at Christmas markets in Europe, primarily in Germany and eastern France. There are non-alcoholic versions of it. Vodka-spiked mulled wine can be found in Polish Christmas markets, where mulled wine is commonly used as a mixer. ",
+    instructions:
+      "Gently rewarm mulled wine in a large pot on the stovetop over low heat, or pour leftovers into your slow cooker and reheat on LOW until warm. If you choose to reheat your leftover refrigerated mulled wine, be aware that the flavors may not be as vibrant as they are when it's freshly made.",
+
+    ingredients1: "Red Wine",
+    ingredients2: "Brandy",
+    ingredients3: "1 large cinnamon stick, or 2 small ones",
+    ingredients4: "2 star anise",
+    ingredients5: "2 strips lemon zest, pared using a vegetable peeler",
+    ingredients6: "4 tbsp caster sugar",
+    season: "winter",
+  },
+
+  {
+    name: "Grapefruit Paloma",
+    id: "6",
+    category: "Coctail",
+    alcoholic: "Alcoholic",
+    image:
+      "https://cdn.vectorstock.com/i/1000x1000/05/74/paloma-cocktail-icon-alcoholic-mixed-drink-vector-44090574.webp",
+    description:
+      "Little is known about the historical origin of the Paloma, the most popular tequila-based cocktail in Mexico. Some believe that it is named after La Paloma (“The Dove”), the popular folk song composed in the early 1860s. Diffords guide states it was created by the legendary Don Javier Delgado Corona, owner and bartender of La Capilla, in Tequila, Mexico.",
+    instructions:
+      "Looking for something to drink on a sunny spring day…tequila with freshly squeezed grapefruit juice and mixed with a bit of lime juice and agave to round everything off…you have a real crowd pleaser. A touch of kosher salt on the rim of the glass will add a little extra touch.",
+
+    ingredients1: "Tequila ",
+    ingredients2: "50ml freshly squeezed pink grapefruit juice",
+    ingredients3: "15ml freshly squeezed lime",
+    ingredients4: "15ml agave nectar",
+    ingredients5: "Top up with Grapefruit soda water",
+    ingredients6: "4 tbsp caster sugar",
+    season: "spring",
   },
 ];
 
